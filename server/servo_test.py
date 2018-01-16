a0de2b3cca015f260d4ba95d26f17a0c225010df
SunFounder <service@sunfounder.com>
2018-01-16 15:57:33 +0800

    add servo_test.py

---
+++ server/servo_test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import PCA9685 as servo
+import time                  # Import necessary modules
+
+MinPulse = 200
+MaxPulse = 700
+
+def setup():
+    pwm = servo.PWM()
+
+def servo_test():
+    for value in range(MinPulse, MaxPulse):
+        pwm.write(0, 0, value)
+        pwm.write(14, 0, value)
+        pwm.write(15, 0, value)
+        time.sleep(0.002)
+
+if __name__ == '__main__':
+    setup()
+    servo_test()
